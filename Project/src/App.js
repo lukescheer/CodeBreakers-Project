f39e2be17329a63db5010481438c2ee4c411929d
Joseph Busse <bussej@purdue.edu>
2019-02-08 16:01:39 -0500

    Couple changes
    just a few changes

--- Project/src/App.js
+++ Project/src/App.js
@@ -4,18 +4,27 @@ import './App.css';
 import { BrowserRouter as Router} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 
+const User = () => {
+  return (<h1>Welcome User</h1>)
+}
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Route path="/" render={
+          <Route path="/" exact render={
               () => {
                 return ( <h1>Hello World</h1>);
               }
           }/>
 
-          <Route path="/signup" render={
+          <Route path="/signup/" exact render={
+              () => {
+                return ( <h1>Signup Page</h1>);
+              }
+          }/>
+          <Route path="/user" exact ={
               () => {
                 return ( <h1>Signup Page</h1>);
               }
