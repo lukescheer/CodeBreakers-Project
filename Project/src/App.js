63373d507d65b9b0f7d0c5fc913c465f3c6b3f27
Joseph Busse <31547084+joebuss@users.noreply.github.com>
2019-02-08 18:42:58 -0500

    Put button and a couple things on
    Spent about 2.5 hours working on state handlers and linking. Going to take a break and learn more before continuing

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Link, NavLink, Redirect} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 
-const User = () => {
-  return (<h1>Welcome User</h1>)
+const User = ({params}) => {
+  return (<h1>Welcome User {params.username} </h1>)
 }
 
 class App extends Component {
+  state = {
+    loggedIn:false
+  }
+  
+  signUpHandle = () => {
+    this.setState({loggedIn:true})
+  }
   render() {
     return (
       <Router>
         <div className="App">
+          <ul>
+            <li>
+              <NavLink to="/" exact activeStyle={
+                {color: 'blue'}
+              }>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup/" exact activeStyle={
+                {color: 'blue'}
+              }>Sign Up</NavLink>
+            </li>
+          </ul>
           <Route path="/" exact render={
               () => {
                 return ( <h1>Hello World</h1>);
               }
           }/>
-
           <Route path="/signup/" exact render={
               () => {
                 return ( <h1>Signup Page</h1>);
               }
           }/>
-          <Route path="/user" exact ={
-              () => {
-                return ( <h1>Signup Page</h1>);
-              }
-          }/>
+
+          <input type="button" value="Sign Up" onClick to="/signup/" />
+          
         </div>
       </Router>
     );
