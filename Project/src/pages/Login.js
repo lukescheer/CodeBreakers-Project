d950692029acd6bed56fd7821e1b610b7b687020
singh497 <47434320+singh497@users.noreply.github.com>
2019-02-10 17:28:55 -0500

    Created Login.js
    Created login page (untested)

---
+++ Project/src/pages/Login.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import Popup from "reactjs.popup";
+import { Redirect } from 'react-router-dom';
+//user login
+class Login extends Component
+{
+    constructor()
+    {
+        super()
+        this.state = 
+        {
+            username: '',
+            password: '',
+        };
+        this.handleCheck = this.handleCheck.bind(this);
+    }
+    handleCheck(e)
+    {
+        //console.log("user inputted this data");
+        console.log(this.state);
+        noerror: 0;
+        if(noerror == 0)
+        {
+            Popup.alert('Password does not match username')
+        }
+        else
+        {
+            //log the user in
+            //ie switch user to homepage
+            return <Redirect to = '/target_homepage'/>
+        }
+    }
+    
+    render() {
+        return (
+            <div className="Formcenter">
+                
+                <h1>Login</h1>
+                
+                <form onSubmit={this.handleCheck} className="FormFields">
+                    <div className="FormField">
+                        <label className="FormField__Lable" htmlFor="username">Username</label>
+                        <input type="username" id="username" className="FormField__Input" placeholder="Enter Your Username" name="username" value={this.state.username} onChange={this.handleChange} />
+                    </div>
+                    <div className="FormField">
+                        <label className="FormField__Lable" htmlFor="password">Password</label>
+                        <input type="password" id="password" className="FormField__Input" placeholder="Enter Your Password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    </div>
+                    <div className="FormField">
+                        <button className="FormField__Button mr-20">Login</button>
+                    </div>
+
+                </form>
+            </div>
+        );
+    }
+}
+export default Login
