943449ef047a290ea044e79a17dc15b3a3ed818b
Elliot Cubit <The--Joke@users.noreply.github.com>
2019-02-09 20:19:34 -0500

    Basic layout for home page
    - Added Container and Rows/Cols for stuff
    - Skeleton for PostPreview
    - Skeleton for Scrolling list container

--- Project/src/Home.js
+++ Project/src/Home.js
@@ -1,21 +1,70 @@
 import React, {Component} from 'react';
+import {Container, Col, Row} from 'react-bootstrap'
+
 
 // Stuff relating to the home page
 
 class Home extends Component {
 
   // Run when this is loaded.
-  componentDidMount(){
+  componentDidMount() {
     document.title = "CodeBreakers"
   }
 
   render() {
     return (
-      <div>
-        <h1> Hello, world! </h1>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+
+          Recommended Posts
+
+
+          </Col>
+
+          <Col>
+          Your Posts
+
+          </Col>
+
+          <Col>
+
+            <Row>
+
+            Your Profile
+
+            </Row>
+            
+            <Row>
+            Messages
+            </Row>
+
+          </Col>
+        </Row>
+      </Container>
     );
   }
+}
+
+// Class for scrolling lists, used in post lists + messages
+class ScrollingList extends Component {
+    constructor(props){
+      super(props);
+    }
+
+    render(){
+      return (
+        <h1> scrolling list </h1>
+      )
+    }
+}
+
+// Defines one element in post list
+function PostPreview(props){
+
+  return (
+    <h1> post </h1>
+  )
 }
 
 
