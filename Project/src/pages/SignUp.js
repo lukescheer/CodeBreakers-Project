6aba4a13aab244fdfa6a6abd632f278d82b167c9
Joseph Busse <31547084+joebuss@users.noreply.github.com>
2019-02-10 00:18:33 -0500

    UI for sign up mostly complete
    Only mostly complete because am waiting on a few other things to be finished. Also added in some sample CSS just to test how it works

---
+++ Project/src/pages/SignUp.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+// Sign up page
+
+class SignUp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            username: '',
+            password: '',
+            //agreement: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        let target = e.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+
+        console.log('This form was submitted with the data:');
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div className="FormCenter">
+                
+                <h1>Signup Page</h1>
+                
+                <form onSubmit={this.handleSubmit} className="FormFields">
+                    <div className="FormField">
+                        <label className="FormField__Lable" htmlFor="email">E-Mail Address</label>
+                        <input type="email" id="email" className="FormField__Input" placeholder="Enter Your Email" name="email" value={this.state.email} onChange={this.handleChange} />
+                    </div>
+                    <div className="FormField">
+                        <label className="FormField__Lable" htmlFor="username">Username</label>
+                        <input type="username" id="username" className="FormField__Input" placeholder="Enter Your Username" name="username" value={this.state.username} onChange={this.handleChange} />
+                    </div>
+                    <div className="FormField">
+                        <label className="FormField__Lable" htmlFor="password">Password</label>
+                        <input type="password" id="password" className="FormField__Input" placeholder="Enter Your Password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    </div>
+                    <div className="FormField">
+                        <button className="FormField__Button mr-20">Sign Up!</button>
+                    </div>
+
+                </form>
+            </div>
+        );
+    }
+}
+export default SignUp;
