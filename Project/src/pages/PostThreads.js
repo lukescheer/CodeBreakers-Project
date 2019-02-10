395476999b58b5312482207bb4af6e91c1e7427b
clabianc <cjlabianca99@gmail.com>
2019-02-10 17:48:12 -0500

    Created PostThread.js
    Started file to handle frontend for posts

---
+++ Project/src/pages/PostThreads.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostThread extends Component {
+   render(){
+      return (
+         <div className="PostThread">
+            <h1>Post Thread </h1>
+         </div>
+      )
+   }
+}
