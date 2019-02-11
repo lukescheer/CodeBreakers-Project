b58db572e51ab5ae5fe40aca13e8931ca3449502
clabianc <cjlabianca99@gmail.com>
2019-02-10 19:55:29 -0500

    Added basic blocks of the post

--- Project/src/pages/PostThreads.js
+++ Project/src/pages/PostThreads.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 class PostThread extends Component {
    render(){
       return (
          <div className="PostThread">
             <h1>Post Thread </h1>
+            <Row> 
+               <Col>
+                  <div className="PostTitle"> 
+                     Post Title
+                  </div>
+               </Col>
+               <Col>
+                  <div className="PostCreator">
+                     Post Creator
+                  </div>
+               </Col>
+            </Row>
+
+            <Row>
+               <div className="InitialPost">
+                  This is the contents of the question
+               </div>   
+            </Row>
+
+            <Row>
+               <div className="Reply1">
+                  This is the contents of the reply
+               </div>
+            </Row>
          </div>
       )
    }
