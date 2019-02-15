59c7f75fb1ca024bf1e5ed886e5ea2246d9b757d
Joseph Busse <31547084+joebuss@users.noreply.github.com>
2019-02-15 12:00:23 -0500

    Started Profile.js
    Created Profile.js, afterwords will do settings

---
+++ Project/src/pages/Profile.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Col, Row} from 'react-bootstrap'
+
+
+//profile page
+
+class Profile extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div style={{height:"150vh"}}>
+            
+            </div>
+        );
+    }
+}
+export default Profile
