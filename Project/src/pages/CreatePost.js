7e55b11cd968e417813c4ae56daee983c7e33b00
Elliot Cubit <The--Joke@users.noreply.github.com>
2019-02-16 16:47:26 -0500

    createPost UI

--- Project/src/pages/CreatePost.js
+++ Project/src/pages/CreatePost.js
@@ -1,4 +1,10 @@
 import React, {Component} from 'react';
+import {Form, Button} from 'react-bootstrap';
+import '../css/createPost.css'
+
+/*
+  Apparently unnecessary
+*/
 
 /*
   True if post ID exists in databse
@@ -11,6 +17,7 @@ function IDIsTaken(){
 /*
   Returns an unused 11 digit base 32 post ID.
   This is a big number - this loop won't take long.
+
 */
 function randomPostID(){
   var num = Math.random().toString(36).substr(2, 11);
@@ -20,6 +27,62 @@ function randomPostID(){
   }
 
   return num;
+}
+
+class PostForm extends Component {
+    constructor(props){
+      super(props);
+      this.state = {title: '', body: '', link:''};
+
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event){
+      const target = event.target;
+
+    }
+
+    handleSubmit(event){
+
+    }
+
+    render() {
+      return (
+
+        <div className="form-wrapper">
+          <h3>
+          Create a New Post
+          </h3>
+
+          <Form>
+            <Form.Row>
+              <Form.Group controlId="formTitle">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" placeholder="An Interesting Title" />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group controlId="formLink">
+                <Form.Label>Link to Code</Form.Label>
+                <Form.Control type="text" placeholder="Github Link" />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group controlId="formTitle">
+                <Form.Label>Description</Form.Label>
+                <Form.Control as="textarea" placeholder="Provide as many details as necessary" />
+              </Form.Group>
+            </Form.Row>
+
+            <Button type="submit">Create Post</Button>
+
+          </Form>
+        </div>
+      );
+    }
 }
 
 class CreatePost extends Component {
@@ -34,10 +97,11 @@ class CreatePost extends Component {
 
   render(){
     return (
-      <div> {randomPostID()}</div>
+      <div>
+        <PostForm />
+      </div>
     );
   }
-
 
 }
 
