536ff6b0ef3052b5b518a853910427f4489451bb
clabianc <cjlabianca99@gmail.com>
2019-02-21 13:53:44 -0500

    Added backend schema for posting

---
+++ Project/backend/post.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+let postSchema = new mongoose.Schema({
+   text: String,
+   author: String,
+   github: String,
+   replies: Array
+})
+
+let replySchema = new mongoose.Schema({
+   text: String,
+   author: String
+})
+
+module.exports = mongoose.model('Post', postSchema);
